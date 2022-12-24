3a81fdf73cf9cd7d08a966f89720ae71189dd747
shrilinge <115638066+shrilinge@users.noreply.github.com>
2022-12-24 13:41:33 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import Head from 'next/head'
+dsimport Head from 'next/head'
 import Header from '@components/Header'
 import Footer from '@components/Footer'
 
@@ -6,12 +6,12 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>Edufirst Computer Insitute</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="Welcome Edufirst Computer Insitute" />
         <p className="description">
           Get started by editing <code>pages/index.js</code>
         </p>
